27132363fe733a440ab23fc4673159eab127c49a
Fabrice Magnan <magnan.fabrice@gmail.com>
2024-01-03 22:17:36 +0100

    optimisation des images compression + format webp pour le background

--- argentbank/src/assets/images.js
+++ argentbank/src/assets/images.js
@@ -1,5 +1,5 @@
 import argentbankLogo from "../assets/img/argentBankLogo.png";
-import banktreeImage from "./img/bank-tree.jpeg";
+import banktreeImage from "./img/bank-tree.webp";
 import chatLogo from "./img/icon-chat.png";
 import moneyLogo from "./img/icon-money.png";
 import securityLogo from "./img/icon-security.png";
